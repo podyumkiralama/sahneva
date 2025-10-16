9e201f676c5b8870476c0967d1dc134f1f94774f
Murat <kasimrize@gmail.com>
2025-10-16 22:47:28 +0300

    Update robots.js

--- app/robots.js
+++ app/robots.js
@@ -1,7 +1,8 @@
 // app/robots.js
 export default function robots() {
   return {
-    rules: [{ userAgent: "*", allow: "/" }],
-    sitemap: "https://sahneva.com/sitemap.xml",
+    rules: [{ userAgent: '*', allow: '/' }],
+    sitemap: 'https://www.sahneva.com/sitemap.xml',
+    host: 'https://www.sahneva.com',
   };
 }

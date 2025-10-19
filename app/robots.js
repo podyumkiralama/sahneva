c8250bf434cd1d22d8c8d7ec0b3db4d3369766a2
Murat <kasimrize@gmail.com>
2025-10-19 10:59:06 +0300

    Update robots.js

--- app/robots.js
+++ app/robots.js
@@ -1,8 +1,17 @@
 // app/robots.js
 export default function robots() {
+  const host = "https://www.sahneva.com";
   return {
-    rules: [{ userAgent: '*', allow: '/' }],
-    sitemap: 'https://www.sahneva.com/sitemap.xml',
-    host: 'https://www.sahneva.com',
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      // Disallow örnekleri (gerekirse):
+      // disallow: ["/api/", "/_next/", "/private/"],
+    },
+    sitemap: [
+      `${host}/sitemap.xml`,
+      `${host}/sitemap-images.xml`,
+    ],
+    host,
   };
 }

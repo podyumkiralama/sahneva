9c96c60f337f27a9062d753aed82a2816a7f49f8
Murat <kasimrize@gmail.com>
2025-10-28 14:45:05 +0300

    Fix hero blur placeholder data

--- app/(site)/page.js
+++ app/(site)/page.js
@@ -6,6 +6,8 @@ import { Suspense } from "react";
 import heroBg from "@/public/img/hero-bg.webp";
 import heroBgBlur from "@/public/img/hero-bg-blur.webp";
 
+const heroPlaceholder = heroBgBlur.blurDataURL ?? heroBgBlur.src;
+
 import CorporateEvents from "../../components/CorporateEvents";
 import Faq from "../../components/Faq";
 import HeroCtasClient from "../../components/HeroCtasClient";
@@ -120,7 +122,7 @@ export default function HomePage() {
           decoding="async"
           sizes="100vw"
           placeholder="blur"
-          blurDataURL={heroBgBlur.src}
+          blurDataURL={heroPlaceholder}
           className="object-cover"
         />
         <div
